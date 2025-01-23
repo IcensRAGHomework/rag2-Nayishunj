5539f310b3ded6009b388336bcbeab2959923cc3
Nayishunj <144424957+Nayishunj@users.noreply.github.com>
2025-01-23 18:39:14 +0800

    do hw02

--- student_assignment.py
+++ student_assignment.py
@@ -7,7 +7,29 @@ q2_pdf = "勞動基準法.pdf"
 
 
 def hw02_1(q1_pdf):
-    pass
+    loader = PyPDFLoader(q1_pdf)
+    documents = loader.load()
+
+    text_splitter = CharacterTextSplitter(chunk_size=500, chunk_overlap=0)
+    docs = text_splitter.split_documents(documents)
+    last_chunk = docs[-1]
+    print("page count={}, last_chunk={}".format(len(docs), last_chunk))
+    return last_chunk
 
 def hw02_2(q2_pdf):
-    pass
+    loader = PyPDFLoader(q2_pdf)
+    documents = loader.load()
+    pdf_text = "\n".join([doc.page_content for doc in documents])
+    separators=["第 .{1,2} 章", "第 .{1,5} 條"]
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1,
+                                                   chunk_overlap=0,
+                                                   keep_separator = True,
+                                                   is_separator_regex=True,
+                                                   separators=separators)
+    chunks = text_splitter.split_text(pdf_text)
+    chunk_count = len(chunks)
+    print("chunk_count={}".format(chunk_count))
+    for i, v in enumerate(chunks):
+        pass
+        #print("chunk {}={}".format(i, v[:20]))
+    return chunk_count
